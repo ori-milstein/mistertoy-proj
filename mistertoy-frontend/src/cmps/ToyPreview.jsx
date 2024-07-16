0f23de844bbc5105b7c099df3d7b77a45a58ecd8
ori-milstein <mydoal@keemail.me>
2024-07-16 21:05:04 +0300

    Refactor toy preview

--- mistertoy-frontend/src/cmps/ToyPreview.jsx
+++ mistertoy-frontend/src/cmps/ToyPreview.jsx
@@ -3,8 +3,7 @@ import { Link } from "react-router-dom";
 export function ToyPreview({ toy }) {
     return (
         <article>
-            <h4>{toy.vendor}</h4>
-            <h1>⛐</h1>
+            <h4>{toy.name}</h4>
             <p>Price: <span>${toy.price.toLocaleString()}</span></p>
             {toy.owner && <p>Owner: <Link to={`/user/${toy.owner._id}`}>{toy.owner.fullname}</Link></p>}
             <hr />
